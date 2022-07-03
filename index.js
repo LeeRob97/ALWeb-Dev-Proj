41d066d0db0be7f80a083b9069b9035d30a5e4d5
Gur <53267268+MomoGur@users.noreply.github.com>
2022-07-02 21:34:37 -0500

    Input requirement added
    Each input needs to be filled in to continue

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function User(username, firstName, lastName) {
     this.id = Math.floor(Math.random() * 100);
-    this.username = username;
+    this.userName = userName;
     this.firstName = firstName;
     this.lastName = lastName;
 }
@@ -9,10 +9,19 @@ window.addEventListener("load", function () {
 
     document.getElementById("submit").addEventListener("click", function() {
         // Triggered when the user clicks submit
-		if(username == undefined){
-			throw('Value1 is a required field for function foo');
-		}
 		
+		// The variables of each input
+		var uName = document.getElementById("userName").value;
+		var fName = document.getElementById("firstName").value;
+		var lName = document.getElementById("lastName").value;
+		
+		if (uName.length == 0 || fName.length == 0 || lName.length == 0){
+			document.getElementById("error").innerHTML = "Please complete form";
+		}
+		else{
+			//To take out the error if there was one prior
+			document.getElementById("error").innerHTML = "";
+		}
 		
     });
 })
